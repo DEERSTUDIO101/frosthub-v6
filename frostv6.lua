0f9746eba04f34c11ec8cab8ce0c0fa1829318d6
Chill Phonk <alibaba978677@gmail.com>
2025-11-06 22:45:04 +0100

    Remove UserInputService import from frostv6.lua

--- frostv6.lua
+++ frostv6.lua
@@ -1,4 +1,3 @@
-local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
 local LocalPlayer = game:GetService("Players").LocalPlayer
